bfb456573367d610811ab9fe495c48e301e22d6f
sakshmaverma72 <sakshmaverma72@gmail.com>
2022-11-14 13:43:51 +0530

    m

--- JS/signup.js
+++ JS/signup.js
@@ -1,3 +1,3 @@
 document.querySelector("#nav1img").addEventListener("click",function(){
-    window.location.href = "http://127.0.0.1:5500/index.html";
+    window.location.href = "./index.html";
 });
